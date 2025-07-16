386f57dffdea0f9e02dd8ca81e8e2c8fae66c355
kyloush <kylegujadur@gmail.com>
2025-07-16 14:22:47 +0400

    3008

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -18,9 +18,9 @@ class Rectangle:
     def width(self, value):
         """Set the width with validation."""
         if not isinstance(value, int):
-            raise TypeError('width must be an integer')
+            raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError('width must be >= 0')
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
@@ -32,7 +32,7 @@ class Rectangle:
     def height(self, value):
         """Set the height with validation."""
         if not isinstance(value, int):
-            raise TypeError('height must be an integer')
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError('height must be >= 0')
+            raise ValueError("height must be >= 0")
         self.__height = value
