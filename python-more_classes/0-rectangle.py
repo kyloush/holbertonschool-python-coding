282bc641088852aa4cb8430ea1aefbf640bf91fd
kyloush <kylegujadur@gmail.com>
2025-07-17 10:00:33 +0400

    0

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-"""This module defines a Rectangle class."""
+"""This module defines a Rectangle class with width and height properties,
+and methods to compute area and perimeter.
+"""
 
 
 class Rectangle:
-    """Defines a rectangle by width and height."""
+    """A class that defines a rectangle by its width and height."""
 
-    def __init__(self, width=0):
+    def __init__(self, width=0, height=0):
         self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -15,15 +18,12 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        """Set the width with validation."""
+        """Set the width of the rectangle with validation."""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-
-    def __init__(self, height=0):
-        self.height = height
 
     @property
     def height(self):
@@ -32,7 +32,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        """Set the height with validation."""
+        """Set the height of the rectangle with validation."""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
