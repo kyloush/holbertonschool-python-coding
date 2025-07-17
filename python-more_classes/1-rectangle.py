c7f7352d78d8a4d97cb89ff567bc73d384999d1e
kyloush <kylegujadur@gmail.com>
2025-07-17 13:22:59 +0400

    without the check code

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -42,16 +42,3 @@ class rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
-
-
-
-Rectangle = rectangle
-
-my_rectangle = Rectangle(2, 4)
-print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
-
-print("--")
-
-my_rectangle.width = 10
-my_rectangle.height = 3
-print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -52,19 +52,3 @@ class rectangle:
         if self.__width == 0 or self.__height == 0:
             return ""
         return "\n".join(["#" * self.__width for _ in range(self.__height)])
-
-
-Rectangle = rectangle
-
-my_rectangle = Rectangle(2, 4)
-print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
-
-print(str(my_rectangle))
-print(repr(my_rectangle))
-
-print("--")
-
-my_rectangle.width = 10
-my_rectangle.height = 3
-print(my_rectangle)
-print(repr(my_rectangle))
